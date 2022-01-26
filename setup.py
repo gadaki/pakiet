8e77289fd8ba6740135262def4cfe658dd20486b
gadaki <85584690+gadaki@users.noreply.github.com>
2022-01-26 19:44:52 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='dataparser',
-    version="2.0.0 TURBO",
+    version='2.0.0',
     url='Dawid,Ewa',
     author='Dawid Michalik, Ewa Mardeusz',
     author_email='263329@student.pwr.edu.pl,263328@student.pwr.edu.pl',
